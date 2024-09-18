4ae8854e748fc0309d76fd35529d10b3fcc74c5a
Artyom Borissov <somebigelephant@gmail.com>
2024-09-18 16:10:47 +0200

    wip

--- terraform/modules/timeweb-cloud/cloud-server/main.tf
+++ terraform/modules/timeweb-cloud/cloud-server/main.tf
@@ -10,8 +10,17 @@ resource "twc_server" "server" {
     ram = var.ram
   }
 
-  local_network {
-    id = var.vpc_id
+  # local_network {
+  #   id = var.vpc_id
+  # }
+
+    # Conditionally add the local_network block if var.vpc_id is set
+  dynamic "local_network" {
+    for_each = var.vpc_id != "" ? [var.vpc_id] : []
+
+    content {
+      id = local_network.value
+    }
   }
 
   project_id = data.twc_projects.project.id
