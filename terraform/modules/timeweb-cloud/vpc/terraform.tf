b02e5b68c972f4393470388879c29d147cadfd68
Artyom Borissov <somebigelephant@gmail.com>
2024-09-18 15:42:00 +0200

    added terraform section

---
+++ terraform/modules/timeweb-cloud/vpc/terraform.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    timeweb-cloud = {
+      source  = "tf.timeweb.cloud/timeweb-cloud"
+      version = "~> 1.3.8"  # Ensure this matches the version you want to use
+    }
+  }
+}
